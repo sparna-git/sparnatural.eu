4ed62f0c94746ed702f465e8ecf8dc96bc582982
FRANCART Thomas <thomas.francart@ext.europarl.europa.eu>
2022-10-19 15:00:05 +0200

    second sample query

--- demos/demo-smt-med/sampleQueries.js
+++ demos/demo-smt-med/sampleQueries.js
@@ -37,5 +37,45 @@ var sampleQueries = {
 	      "children": []
 	    }
 	  ]
+	},
+	"example.2":
+	{
+	  "distinct": true,
+	  "variables": [
+	    "?this",
+	    "?UniteCommuneDeDispensation_1"
+	  ],
+	  "defaultLang": "fr",
+	  "order": null,
+	  "branches": [
+	    {
+	      "line": {
+	        "s": "?this",
+	        "p": "http://data.esante.gouv.fr/ansm/medicament/dispenseDans",
+	        "o": "?UniteCommuneDeDispensation_1",
+	        "sType": "http://data.esante.gouv.fr/ansm/medicament/SpecialitePharmaceutique",
+	        "oType": "http://data.esante.gouv.fr/ansm/medicament/UniteCommuneDeDispensation",
+	        "values": []
+	      },
+	      "children": [
+	        {
+	          "line": {
+	            "s": "?UniteCommuneDeDispensation_1",
+	            "p": "http://data.esante.gouv.fr/ansm/medicament/classementSubstanceDosageForme",
+	            "o": "?MedicamentParSubstanceDosageForme_2",
+	            "sType": "http://data.esante.gouv.fr/ansm/medicament/UniteCommuneDeDispensation",
+	            "oType": "http://data.esante.gouv.fr/ansm/medicament/MedicamentParSubstanceDosageForme",
+	            "values": [
+	              {
+	                "label": "acide clavulanique 50 mg + amoxicilline 500 mg solution injectable/pour perfusion (2)",
+	                "uri": "http://data.esante.gouv.fr/ansm/medicament/PHPID4_1"
+	              }
+	            ]
+	          },
+	          "children": []
+	        }
+	      ]
+	    }
+	  ]
 	}
 };
