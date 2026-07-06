a78d44e0915f1ac9b64ce5421fa841b74afa3dde
Thomas Francart <thomas.francart@sparna.fr>
2026-07-06 19:31:21 +0200

    update demo-nakala

--- demos/demo-nakala/sampleQueries.js
+++ demos/demo-nakala/sampleQueries.js
@@ -17,12 +17,14 @@ var sampleQueries = {
         "o": "ZZ_Other_2",
         "sType": "https://shacl-play.sparna.fr/shapes/Aggregation",
         "oType": "http://special/ZZ_Other",
-        "values": [
+        "criterias": [
           {
             "label": "https://creativecommons.org/licenses/by/4.0/",
-            "rdfTerm": {
-              "type": "uri",
-              "value": "https://creativecommons.org/licenses/by/4.0/"
+            "criteria": {
+              "rdfTerm": {
+                "type": "uri",
+                "value": "https://creativecommons.org/licenses/by/4.0/"
+              }
             }
           }
         ]
@@ -143,30 +145,32 @@ var sampleQueries = {
             "o": "Z_Location_4",
             "sType": "https://shacl-play.sparna.fr/shapes/Place",
             "oType": "http://special/Z_Location",
-            "values": [
+            "criterias": [
               {
                 "label": "<svg id=\"svgelem\" width=\"30\" height=\"30\" viewBox=\"0 0 2.768554687500001 2.6751296840457783\" xmlns=\"http://www.w3.org/2000/svg\" style=\" transform: rotateX(180deg);\" preserveAspectRatio=\"xMidYMid meet\">   <g><polygon points=\"0,0 0,2.6751296840457783 2.768554687500001,2.6751296840457783 2.768554687500001,0\" style=\"fill:#ffffff;\" /></g></svg><span>90834 km²</span>",
-                "type": "Rectangle",
-                "coordinates": [
-                  [
-                    {
-                      "lat": 42.90816007196054,
-                      "lng": 5.858641862869264
-                    },
-                    {
-                      "lat": 45.583289756006316,
-                      "lng": 5.858641862869264
-                    },
-                    {
-                      "lat": 45.583289756006316,
-                      "lng": 8.627196550369264
-                    },
-                    {
-                      "lat": 42.90816007196054,
-                      "lng": 8.627196550369264
-                    }
+                "criteria": {
+                  "type": "Rectangle",
+                  "coordinates": [
+                    [
+                      {
+                        "lat": 42.90816007196054,
+                        "lng": 5.858641862869264
+                      },
+                      {
+                        "lat": 45.583289756006316,
+                        "lng": 5.858641862869264
+                      },
+                      {
+                        "lat": 45.583289756006316,
+                        "lng": 8.627196550369264
+                      },
+                      {
+                        "lat": 42.90816007196054,
+                        "lng": 8.627196550369264
+                      }
+                    ]
                   ]
-                ]
+                }
               }
             ]
           },
